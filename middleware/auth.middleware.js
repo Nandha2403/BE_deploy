96c68d7a1935e855afb2a350c6ebaf3b0fce592a
NandhaRaghu <nandhuvj451@gmail.com>
2023-06-09 23:13:40 +0530

    secret updated

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config()
 
 const auth = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
   if (token) {
     try {
-      const decoded = jwt.verify(token, "masai");
+      const decoded = jwt.verify(token, process.env.secret);
       if (decoded) {
         console.log(decoded);
         req.body.userID=decoded.userID
